3861e2cf28b30282e74bddded1f02f3476e0925b
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-05-27 19:40:25 -0300

    :sparkles: feat: Creada tela amigos

---
+++ src/Page/Friends/index.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { CardMain } from "../../Components/CardMain";
+import { CardSecondary } from "../../Components/CardSecondary";
+import { Pagination } from "../../Components/Pagination";
+import { UserLayout } from "../../Layout/User";
+export const FriendPage = () => {
+  return (
+    <UserLayout lateral={<Lateral />} >
+      <CardMain title="Amigos" count={2000} pagination={<Pagination/>}>
+        {[...Array(20)].map((_, key) => (
+          <CardSecondary
+            key={key}
+            round
+            text="Bill Gates"
+            src="https://cdn.allfamous.org/people/avatars/bill-gates-zdrr-allfamous.org.jpg"
+          />
+        ))}
+        
+      </CardMain>
+    </UserLayout>
+  );
+};
+
+export const Lateral = () => {
+  return (
+    <CardMain title="Comunidades" count={1000} toAll='communities' >
+      {[...Array(8)].map((_, key) => (
+        <CardSecondary
+          size="md"
+          key={key}
+          text="Full Bugs"
+          src="https://thumbs.dreamstime.com/b/o-homem-irado-na-camisa-vermelha-rasga-folha-de-papel-6582601.jpg"
+        />
+      ))}
+    </CardMain>
+  );
+};
