062acd29a573529efbc4fb581e9617b0bcb5b018
sabrinamaral <sabrinamaral@hotmail.com>
2022-05-30 23:08:59 -0300

    centraliza cards main card

--- src/Components/CardMain/cardMain.styled.js
+++ src/Components/CardMain/cardMain.styled.js
@@ -19,25 +19,26 @@ export const CardContainer = styled(Card)`
       background-color: var(--pink) !important;
     }
   }
-  .card-body{
+  .card-body {
     display: flex;
-    gap:5px;
-    flex-wrap:wrap;
+    justify-content: center;
+    gap: 5px;
+    flex-wrap: wrap;
   }
-  .card-footer{
-    background:var(--white) ;
-    border-top:0px;
-    padding: 5px 10px ;
-    text-align:end ;
-    a{
+  .card-footer {
+    background: var(--white);
+    border-top: 0px;
+    padding: 5px 10px;
+    text-align: end;
+    a {
       color: var(--pink);
     }
   }
-  .active >.page-link {
-    background:var(--pink) ;
+  .active > .page-link {
+    background: var(--pink);
     border: var(--pink);
   }
   .pagination {
-    justify-content:flex-end;
+    justify-content: flex-end;
   }
 `;
