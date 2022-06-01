88b50a7c41d98c27d8f3914025084b40e060e06e
chicodevbr <chicodevbr@gmail.com>
2022-06-01 17:29:43 -0300

    feat: :twisted_rightwards_arrows: resolvendo conflitos

--- src/Components/ProfileAside/ProfileAsideItems.js
+++ src/Components/ProfileAside/ProfileAsideItems.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaUserCircle, FaArchive, FaCamera, FaComment } from 'react-icons/fa';
 import { Nav } from 'react-bootstrap';
-import Button from '../Button';
+import { NewButtonLink } from '../Button';
 
 const ProfileAsideItems = ({
   name,
@@ -20,13 +20,13 @@ const ProfileAsideItems = ({
         <li>{local}</li>
       </ul>
       <Nav className="menu-links">
-        <Button to="/" icon={<FaUserCircle />} name="Perfil" />
-        <Button to="/friends" icon={<FaComment />} name="Recados" />
-        <Button to="/communities" icon={<FaCamera />} name="Fotos" />
-        <Button to="/" icon={<FaArchive />} name="Depoimentos" />
+        <NewButtonLink to="/" icon={<FaUserCircle />} name="Perfil" />
+        <NewButtonLink to="/friends" icon={<FaComment />} name="Recados" />
+        <NewButtonLink to="/communities" icon={<FaCamera />} name="Fotos" />
+        <NewButtonLink to="/" icon={<FaArchive />} name="Depoimentos" />
       </Nav>
       <div className="edit-profile">
-        <a href="#">Editar Perfil</a>
+        <a href="/">Editar Perfil</a>
       </div>
     </>
   );
--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,22 +1,22 @@
-import { Col } from "react-bootstrap";
-import Header from "../Components/Header";
-import Profile from "../Components/Profile";
-import * as S from "./style";
+import { Col } from 'react-bootstrap';
+import Header from '../Components/Header';
+import Profile from '../Components/ProfileAside';
+import * as S from './style';
 
-const Layout = ({ children, lateral,centerCol = 6 }) => {
+const Layout = ({ children, lateral, centerCol = 6 }) => {
   return (
     <>
       <Header />
       <S.MainContainer>
         <S.Container>
-          <Col md={2} >
+          <Col md={2}>
             <Profile />
           </Col>
           <Col md={centerCol}>{children}</Col>
-          <Col >{lateral}</Col>
+          <Col>{lateral}</Col>
         </S.Container>
       </S.MainContainer>
     </>
   );
 };
-export default Layout
+export default Layout;
