3d4d31349155c45ee2a68d64c460054f55cd1d67
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-02 18:56:39 -0300

    :heavy_plus_sign: Adicionado lib para carrucel

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { GlobalStyles } from "./Styles/GlobalStyle";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GraphQlProvider } from "./Context/graphQl";
 import { DataProvider } from "./Context/dataContext";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
