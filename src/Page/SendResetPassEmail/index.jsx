363176ce024c1fafcf86b659a037c5b04e6ea9d8
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-04 18:01:43 -0300

    :recycle: refactor: Refatoração de input modal

--- src/Page/SendResetPassEmail/index.jsx
+++ src/Page/SendResetPassEmail/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { StyledInput,  StyledSubmitInput } from "./style";
+import { StyledInput, StyledSubmitInput } from "./style";
 import { SEND_EMAIL } from "../../Graphql/Mutations";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useMutation } from "@apollo/client";
 import { SendEmailForm } from "../Login/style";
+import { NewInputForm } from "../../Components/Input";
 export default function SendResetPassEmail() {
   const { handleSubmit, handleChange, values, touched, errors, handleBlur } =
     useFormik({
@@ -22,41 +23,32 @@ export default function SendResetPassEmail() {
     });
 
   const [SendEmail, { data, loading, erro }] = useMutation(SEND_EMAIL);
- 
-  if (loading) return (
-    <SendEmailForm>
-          <h3>loading</h3>
-        </SendEmailForm>
-  );
+
+  if (loading)
+    return (
+      <SendEmailForm>
+        <h3>loading</h3>
+      </SendEmailForm>
+    );
   if (data)
     return (
-     
-        <SendEmailForm>
-          <h3>{data.sendEmailresetPassword}</h3>
-        </SendEmailForm>
-     
+      <SendEmailForm>
+        <h3>{data.sendEmailresetPassword}</h3>
+      </SendEmailForm>
     );
   if (erro) return <h1>{erro}</h1>;
 
   return (
-  
-      <SendEmailForm onSubmit={handleSubmit}>
-       
-        <div>
-          <label htmlFor="email">E-mail</label>
-          <StyledInput
-            onBlur={handleBlur}
-            onChange={handleChange}
-            value={values.email}
-            name="email"
-            placeholder="Email"
-            type="text"
-          />
-          {touched.email && errors.email ? <p>{errors.email}</p> : null}
-        </div>
-        <StyledSubmitInput value="Enviar instruções" type={"submit"} />
-      
-      </SendEmailForm>
-    
+    <SendEmailForm onSubmit={handleSubmit}>
+      <NewInputForm
+        name="email"
+        label="Email"
+        value={values.email}
+        onChange={handleChange}
+        isValid={touched.email && !errors.email}
+        error={errors.email}
+      />
+      <StyledSubmitInput value="Enviar instruções" type={"submit"} />
+    </SendEmailForm>
   );
 }
