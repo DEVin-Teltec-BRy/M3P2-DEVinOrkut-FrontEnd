85f19dace58c427ef1f3f7661c4d39519d32c7b0
Daniel Meireles <httpdaniel@yahoo.com.br>
2022-06-04 19:52:53 -0300

    Trabalhando na feature

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -2,12 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useData } from "../Context/dataContext";
 
 const PrivateRoute = ({ children, redirectTo }) => {
-  const { user, handleToken } = useData()
-  if(!user.token && localStorage.getItem("Token")) {
-    handleToken()
-  } else {
-    const isAuthenticated = user.token;
+  const { user } = useData()
+    const isAuthenticated = user.token || localStorage.getItem("Token");
     return isAuthenticated ? children : <Navigate to={redirectTo} />;
-  }
   };
 export default PrivateRoute
