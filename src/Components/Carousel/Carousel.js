36afafc35f1af04183e9dbd39cf248dae9b51610
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-04 17:58:51 -0300

    :recycle: refactor: Corregido color a carrosel

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 
-
-
-
 function BootstrapCarousel({ arrayString }) {
-    return (
-        <Carousel controls= {false} style={{ height: 150, margin:24}}>
-                { arrayString && arrayString.map((text) => ( 
-
-                    
-<Carousel.Item key={text}>
-    <h4>{ text }</h4>   
-</Carousel.Item>
-  
-  ))}
-                          
-</Carousel>
-        
-    )} 
+  return (
+    <Carousel
+      controls={false}
+      style={{ height: 150, margin: 24, color: "#fff" }}
+    >
+      {arrayString &&
+        arrayString.map((text) => (
+          <Carousel.Item key={text}>
+            <h4>{text}</h4>
+          </Carousel.Item>
+        ))}
+    </Carousel>
+  );
+}
 
 export { BootstrapCarousel };
