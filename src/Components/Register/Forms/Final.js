812afe5215a4858627a3ebb43484e6d06ff21864
chicodevbr <chicodevbr@gmail.com>
2022-06-06 09:18:59 -0300

    fix: :bug: retirando imports desnecessários

--- src/Components/Register/Forms/Final.js
+++ src/Components/Register/Forms/Final.js
@@ -4,7 +4,6 @@ import { CREATE_USER_MUTATION } from '../../../Graphql/Mutations/CreateUserMutat
 import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import Spinner from 'react-bootstrap/Spinner';
 import { Loading } from '../../Loading';
 
 const Final = () => {
