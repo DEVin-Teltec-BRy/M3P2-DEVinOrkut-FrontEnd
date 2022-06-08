6bd5484e4adf358c06cb8f4a8879fb0f723c9dd4
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-08 11:34:17 -0300

    feature: Adiciona paginação na página de amigos

--- src/Page/Friends/index.js
+++ src/Page/Friends/index.js
@@ -4,56 +4,46 @@ import { Lateral } from "../../Components/Lateral";
 import { Pagination } from "../../Components/Pagination";
 import { useData } from "../../Context/dataContext";
 import Layout from "../../Layout";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Loading } from "../../Components/Loading";
 
 export const FriendPage = () => {
-  const [testArr, setTestArr] = useState([]);
   const [index, setIndex] = useState(1);
 
   const {
     user: { friends, communities },
   } = useData();
-
-  useEffect(() => {
-    if (friends.length > 0) {
-      const testArrEffect = Array(39).fill(friends[0]);
-      testArrEffect.push(friends[1]);
-      setTestArr(testArrEffect);
-    }
-    console.log(index);
-  }, [friends, index]);
-
-  // console.log("friends: ", friends);
-  // console.log("testArr: ", testArr);
+  console.log(friends);
 
   return (
     <Layout lateral={<Lateral content={communities} title="Comunidades" />}>
       <CardMain
         title="Amigos"
-        count={testArr.length}
+        count={friends.length}
         pagination={
-          <Pagination 
-            pageChange={setIndex}
-            // nro={5} 
-            nro={testArr.length / 20}
+          <Pagination
+            pageChange={setIndex} // Atualiza o índice da páginação
+            nro={friends.length / 20} // Limita o número de usuários por página em 20
           />
         }
       >
-        {testArr.length > 0 ? (
-          testArr.map(({ fullName, id }, key) => (
-            (key < (index)*20 && key >= ((index)*20)-20 ) ?
-            (
-               <CardSecondary
+        {friends.length > 0 ? (
+          friends.map(({ fullName, id, profilePicture }, key) =>
+            key < index * 20 && key >= (index - 1) * 20 ? ( // Mapeia os usuários a serem mostrados de acordo com o índice da página
+              <CardSecondary
                 key={key}
                 round
                 to="usuario"
                 id={id}
                 text={fullName}
-                src="https://cdn.allfamous.org/people/avatars/bill-gates-zdrr-allfamous.org.jpg"
+                src={
+                  profilePicture.length > 0
+                    ? profilePicture[0]
+                    : "https://cdn.allfamous.org/people/avatars/bill-gates-zdrr-allfamous.org.jpg"
+                }
               />
-            ) : (null)
-          ))
+            ) : null
+          )
         ) : (
           <Loading></Loading>
         )}
