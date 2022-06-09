f4d50732c395b949caef8317889fecedaf1af10d
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-08 23:48:39 -0300

    update: atualiza dinamicamente a referencia de amigos e comunidades com base no perfil do usuário

--- src/Components/Lateral/index.js
+++ src/Components/Lateral/index.js
@@ -1,18 +1,31 @@
 import { CardMain } from "../CardMain";
 import { CardSecondary } from "../CardSecondary";
+import { useParams } from "react-router-dom";
 
 export const Lateral = ({ content, title }) => {
-  let toAllUri = title === "Amigos" ? "friends" : "communities";
+  const { id } = useParams();
+  let toAllUri = "";
+  if (id) {
+    toAllUri = title === "Amigos" ? `/user/${id}/friends` : `/user/${id}/communities`;
+  } else {
+    toAllUri = title === "Amigos" ? `friends` : `communities`;
+  }
+
+  console.log("id ", id);
+
+  console.log(content, toAllUri);
   return (
     <CardMain title={title} count={content.length} toAll={toAllUri}>
-      {content.map(({ fullName, id }, key) => (
+      {content.map((data, key) => (
         <CardSecondary
           round
-          to='usuario'
-          id={id}
+          // to='usuario'
+          to={title === "Amigos" ? "usuario" : "comunidade"}
+          id={data.id}
           key={key}
-          text={fullName}
-          src="https://cdn.allfamous.org/people/avatars/bill-gates-zdrr-allfamous.org.jpg"
+          text={title === "Amigos" ? data.fullName : data.name}
+          // text={fullName}
+          src={title === "Amigos" ? data.profilePicture: data.logo}
         />
       ))}
     </CardMain>
