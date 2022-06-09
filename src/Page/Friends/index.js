429ef2eea8fbd19204f7d52779445b51dd8f2b9a
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-09 16:02:41 -0300

    improvement: Adicionado componente loading para evitar quebra da página caso os dados não tenham sido carregados

--- src/Page/Friends/index.js
+++ src/Page/Friends/index.js
@@ -9,45 +9,46 @@ import { Loading } from "../../Components/Loading";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_USER_BY_ID } from "../../Graphql/Querys";
+import { Link } from "react-router-dom";
 
 export const FriendPage = () => {
   const [index, setIndex] = useState(1);
+  const { user } = useData();
   const { id } = useParams();
 
   const { loading, error, data } = useQuery(GET_USER_BY_ID, {
     variables: { userId: id },
   });
 
-  const {
-    user: { friends, communities },
-  } = useData();
+  let userData = null;
 
-  let friendList = [];
-  let communitiesList = [];
-
-  if(id) {
-    friendList = data.user.friends;
-    communitiesList = data.user.communities
+  if (id && data) {
+    userData = data.user;
   } else {
-    friendList = friends;
-    communitiesList = communities;
+    userData = user;
   }
 
-  return (
-    <Layout lateral={<Lateral content={communitiesList} title="Comunidades"/>}>
+  return id && loading ? (
+    <Loading />
+  ) : id && error ? (
+    <Link to="/" />
+  ) : (
+    <Layout
+      lateral={<Lateral content={userData.communities} title="Comunidades" />}
+    >
       <CardMain
         title="Amigos"
-        count={friendList.length}
+        count={userData.friends.length}
         pagination={
           <Pagination
             pageChange={setIndex} // Atualiza o índice da páginação
-            nro={friendList.length / 20} // Limita o número de usuários por página em 20
+            nro={userData.friends.length / 20} // Limita o número de usuários por página em 20
             index={index}
           />
         }
       >
-        {friendList.length > 0 ? (
-          friendList.map(({ fullName, id, profilePicture }, key) =>
+        {userData.friends.length > 0 ? (
+          userData.friends.map(({ fullName, id, profilePicture }, key) =>
             key < index * 20 && key >= (index - 1) * 20 ? ( // Mapeia os usuários a serem mostrados de acordo com o índice da página
               <CardSecondary
                 key={key}
