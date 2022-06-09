d8efc0d33bcc268997af8a0bd8c77f2d40c692b3
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-09 16:05:11 -0300

    update: removido dependências não utilizadas

--- src/Page/Communities/index.js
+++ src/Page/Communities/index.js
@@ -1,7 +1,5 @@
 import { CardMain } from "../../Components/CardMain";
 import { Pagination } from "../../Components/Pagination";
-import ModalComponent from "../../Components/Modal/CreateCommunity";
-import CommunitiesList from "../../Components/CommunitiesList";
 import Layout from "../../Layout";
 import { Lateral } from "../../Components/Lateral";
 import { useData } from "../../Context/dataContext";
