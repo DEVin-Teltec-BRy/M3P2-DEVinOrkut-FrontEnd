83eab86d4a0bec279ba0dd4d1b0bac3be22512f8
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-08 23:47:22 -0300

    update: Indicação de usuário sem amigos

--- src/Page/Friends/index.js
+++ src/Page/Friends/index.js
@@ -6,29 +6,48 @@ import { useData } from "../../Context/dataContext";
 import Layout from "../../Layout";
 import { useState } from "react";
 import { Loading } from "../../Components/Loading";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_USER_BY_ID } from "../../Graphql/Querys";
 
 export const FriendPage = () => {
   const [index, setIndex] = useState(1);
+  const { id } = useParams();
+
+  const { loading, error, data } = useQuery(GET_USER_BY_ID, {
+    variables: { userId: id },
+  });
 
   const {
     user: { friends, communities },
   } = useData();
 
+  let friendList = [];
+  let communitiesList = [];
+
+  if(id) {
+    friendList = data.user.friends;
+    communitiesList = data.user.communities
+  } else {
+    friendList = friends;
+    communitiesList = communities;
+  }
+
   return (
-    <Layout lateral={<Lateral content={communities} title="Comunidades" />}>
+    <Layout lateral={<Lateral content={communitiesList} title="Comunidades"/>}>
       <CardMain
         title="Amigos"
-        count={friends.length}
+        count={friendList.length}
         pagination={
           <Pagination
             pageChange={setIndex} // Atualiza o índice da páginação
-            nro={friends.length / 20} // Limita o número de usuários por página em 20
+            nro={friendList.length / 20} // Limita o número de usuários por página em 20
             index={index}
           />
         }
       >
-        {friends.length > 0 ? (
-          friends.map(({ fullName, id, profilePicture }, key) =>
+        {friendList.length > 0 ? (
+          friendList.map(({ fullName, id, profilePicture }, key) =>
             key < index * 20 && key >= (index - 1) * 20 ? ( // Mapeia os usuários a serem mostrados de acordo com o índice da página
               <CardSecondary
                 key={key}
@@ -45,7 +64,7 @@ export const FriendPage = () => {
             ) : null
           )
         ) : (
-          <Loading></Loading>
+          <span>O usuário não possui nenhum amigo!</span>
         )}
       </CardMain>
     </Layout>
