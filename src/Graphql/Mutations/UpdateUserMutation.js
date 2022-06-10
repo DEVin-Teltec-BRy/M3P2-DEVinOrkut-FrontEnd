5f3ebebe0937c7249eb8cc7e37cef054be06f1d7
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-10 16:27:40 -0300

    add: Criada mutation para atualização de dados do usuário

--- src/Graphql/Mutations/UpdateUserMutation.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client/core';
-
-export const UPDATE_USER_MUTATION = gql`
-  mutation UpdateUser($user: UserInput!) {
-    createUser(user: $user) {
-      token
-    }
-  }
-`;
