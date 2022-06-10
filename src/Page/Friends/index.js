8d0c86ae0e881139856c03b2336eca4e2bb79dfd
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-10 18:35:05 -0300

    :sparkles: feat: Integração de paginação

--- src/Page/Friends/index.js
+++ src/Page/Friends/index.js
@@ -3,49 +3,87 @@ import { CardSecondary } from "../../Components/CardSecondary";
 import { Lateral } from "../../Components/Lateral";
 import { Pagination } from "../../Components/Pagination";
 import Layout from "../../Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Loading } from "../../Components/Loading";
 import { useLocation } from "react-router-dom";
+import { GET_FRIENDS } from "../../Graphql/Querys";
+import { useQuery } from "@apollo/client";
+import { PlaceholderUser } from "../../Components/PlaceHolder";
 
-export const FriendPage = ({visitedData}) => {
-  const [index, setIndex] = useState(1);
+export const FriendPage = ({ visitedData }) => {
+  const location = useLocation();
+  const [listFriend, setList] = useState([]);
+  const nPerPage = 4;
+  const [paginating, setPaginations] = useState({
+    count: 0,
+    currentPage: 1,
+    nextPage: 0,
+    prevPage: 0,
+    totalPages: 0,
+  });
 
-  const location = useLocation()
-  const { friends, communities } = location.state;
+  const { loading, data, refetch} = useQuery(GET_FRIENDS, {
+    variables: {
+      pagination: {
+        page: paginating?.currentPage,
+        per_page: nPerPage,
+      },
+    },
+  });
+  useEffect(() => {
+    
+    if (data) {
+      const { getFriends } = data;
+      const { pagination, results,  } = getFriends;
+      setTimeout(() => {
+        setList(results);
+      }, 2000);
+      setPaginations((_) => pagination);
+    }
+  }, [data]);
 
-  return friends && communities ? (
-    <Layout
-      lateral={<Lateral content={communities} title="Comunidades" />}
-    >
+  const handleNextPage = (page) => {
+    refetch({
+      pagination: {
+        page: page,
+        per_page: nPerPage,
+      },
+    });
+  };
+
+  const { communities } = location.state;
+
+  return listFriend ? (
+    <Layout lateral={<Lateral content={communities} title="Comunidades" />}>
       <CardMain
         title="Amigos"
-        count={friends.length}
+        count={paginating?.count}
         pagination={
           <Pagination
-            pageChange={setIndex} // Atualiza o índice da páginação
-            nro={friends.length / 20} // Limita o número de usuários por página em 20
-            index={index}
+            pageChange={handleNextPage}
+            nro={paginating?.count / nPerPage}
+            index={paginating?.currentPage}
           />
         }
       >
-        {friends.length > 0 ? (
-          friends.map(({ fullName, id, profilePicture }, key) =>
-            key < index * 20 && key >= (index - 1) * 20 ? ( // Mapeia os usuários a serem mostrados de acordo com o índice da página
-              <CardSecondary
-                key={key}
-                round
-                to="usuario"
-                id={id}
-                text={fullName}
-                src={
-                  profilePicture.length > 0
-                    ? profilePicture[0]
-                    : "https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg"
-                }
-              />
-            ) : null
-          )
-        ) : (
+        {loading &&
+          [...Array(4)].map((_, idx) => <PlaceholderUser key={idx} />)}
+        {listFriend?.length > 0 &&
+          listFriend.map(({ fullName, id, profilePicture }, key) => (
+            <CardSecondary
+              key={key}
+              round
+              to="usuario"
+              id={id}
+              text={fullName}
+              src={
+                profilePicture.length > 0
+                  ? profilePicture[0]
+                  : "https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg"
+              }
+            />
+          ))}
+        {listFriend?.length === 0 && !loading && (
           <span>O usuário não possui nenhum amigo!</span>
         )}
       </CardMain>
