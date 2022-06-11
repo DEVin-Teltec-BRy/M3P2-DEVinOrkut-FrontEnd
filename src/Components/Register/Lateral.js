82647222b7410ce512dbf130a179ab3d1f8f3c5c
chicodevbr <chicodevbr@gmail.com>
2022-06-11 09:16:12 -0300

    feat: :sparkles: link para retornar pra página principal

--- src/Components/Register/Lateral.js
+++ src/Components/Register/Lateral.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import LogoLateral from '../../Assets/images/LogoLateral.svg';
 import { Logo } from './style';
 
 const Lateral = () => {
-  return <Logo src={LogoLateral} alt="Logo Lateral" />;
+  return (
+    <Link to="/">
+      <Logo src={LogoLateral} alt="Logo Lateral" />
+    </Link>
+  );
 };
 
 export default Lateral;
