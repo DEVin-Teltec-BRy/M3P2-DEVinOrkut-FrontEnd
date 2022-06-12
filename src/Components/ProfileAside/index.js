8e5b793045f353adbdea1f75aa5a39cbc9b33f18
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-06-12 09:47:35 -0300

    merge with development

--- src/Components/ProfileAside/index.js
+++ src/Components/ProfileAside/index.js
@@ -4,39 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { closeModal, openModal } from '../../Store/rootSlice';
 import ModalUpload from './ModalUpload';
 
-const Profile = () => {
-  const { user: loggedUser } = useData();
-  const { id } = useParams();
-
-  const navigate = useNavigate();
-
-  const { data } = useQuery(GET_USER_BY_ID, {
-    variables: { userId: id },
-    skip: loggedUser.id === id ? true : false,
-  });
-  const [userPage, setUserPage] = useState(null);
-
-  useEffect(() => {
-    if (id === loggedUser.id) {
-      navigate('/');
-    }
-    if (loggedUser && !data) {
-      setUserPage(loggedUser);
-    }
-    if (data) {
-      const { user } = data;
-      setUserPage(user);
-    }
-  }, [
-    navigate,
-    data,
-    id,
-    loggedUser,
-    loggedUser.id,
-    loggedUser?.friendRequest,
-    userPage?.friendRequest,
-  ]);
-
 const Profile = ({visitedData}) => {
   const isOpen = useSelector((state) => state.IsOpen);
   const dispatch = useDispatch();
@@ -45,30 +12,29 @@ const Profile = ({visitedData}) => {
   };
 
   return (
-    <S.ProfileContainer>
-      <ProfileAsideItems
-        key={visitedData.id}
-        name={visitedData.fullName}
-        profilePicture={
-          visitedData.profilePicture.length > 0
-            ? visitedData.profilePicture[0]
-            : 'https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg'
-        }
-        relationship={visitedData.relationship}
-        city={visitedData.city}
-        state={visitedData.state}
-        gender={visitedData.gender}
-        buttonText={visitedData.fullName}
-        onClick={handleModalOpen}
-      />
-      <ModalUpload
-        show={isOpen}
-        onHide={() => {
-          dispatch(closeModal());
-        }}
-      />
-      
-    </S.ProfileContainer>
+      <S.ProfileContainer>
+        <ProfileAsideItems
+            key={visitedData.id}
+            name={visitedData.fullName}
+            profilePicture={
+              visitedData.profilePicture.length > 0
+                  ? visitedData.profilePicture[0]
+                  : 'https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg'
+            }
+            relationship={visitedData.relationship}
+            city={visitedData.city}
+            state={visitedData.state}
+            gender={visitedData.gender}
+            buttonText={visitedData.fullName}
+            onClick={handleModalOpen}
+        />
+        <ModalUpload
+            show={isOpen}
+            onHide={() => {
+              dispatch(closeModal());
+            }}
+        />
+      </S.ProfileContainer>
   )
 };
 
