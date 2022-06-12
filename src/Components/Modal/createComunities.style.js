8a2984b376e82f27104814847f079fed411fe498
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-12 14:03:46 -0300

    :ok_hand: style: Ajuste de dimeção do crear comunide

--- src/Components/Modal/createComunities.style.js
+++ src/Components/Modal/createComunities.style.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const NewCommunities = styled.button`
   width: clamp(7rem, 7.8rem, 10rem);
-  max-width: "7.7rem;";
-  max-height: "8.5rem";
+  height: 8.5rem;
+  max-width: 7.7rem;;
+  max-height: 8.5rem;
   border: none;
   border-radius: 10px;
   font-size: 15px;
