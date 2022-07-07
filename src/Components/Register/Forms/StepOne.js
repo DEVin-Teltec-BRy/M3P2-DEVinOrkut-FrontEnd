b43ab625d41ebbcc3e04abe38cda0c3e1e48e20a
Marcos Mendes <marcos_fmendes@hotmail.com>
2022-07-07 08:43:12 -0300

    refactor: :hammer: exclui variável não utilizadas
    Signed-off-by: Marcos Mendes <marcos_fmendes@hotmail.com>

--- src/Components/Register/Forms/StepOne.js
+++ src/Components/Register/Forms/StepOne.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { formStep, formRegister } from "../../../Store/rootSlice";
 import { validateStepOne } from "../../../Utils/validateForm";
 
